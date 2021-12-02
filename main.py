26fb3414a74f0123daa29487043bfedc5d7737cf
barti132 <barteksredzinski@gmail.com>
2021-12-02 02:41:15 +0100

    add: drawing X and O

--- main.py
+++ main.py
@@ -10,7 +10,12 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('Tic tac toe game')
 
 line_width = 8
-markers = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+markers = [[0, 0, 0],
+           [0, 0, 0],
+           [0, 0, 0]]
+clicked = False
+pos = []
+player = 1
 
 
 def draw_grid():
@@ -22,14 +27,41 @@ def draw_grid():
         pygame.draw.line(screen, grid_color, (x * 200, 0), (x * 200, screen_height), line_width)
 
 
+def draw_markers():
+    x_pos = 0
+    for x in markers:
+        y_pos = 0
+        for y in x:
+            if y == 1:
+                pygame.draw.line(screen, (0, 255, 0), (x_pos * 200 + 15, y_pos * 200 + 15),
+                                 (x_pos * 200 + 185, y_pos * 200 + 185), line_width)
+                pygame.draw.line(screen, (0, 255, 0), (x_pos * 200 + 15, y_pos * 200 + 185),
+                                 (x_pos * 200 + 185, y_pos * 200 + 15), line_width)
+            if y == -1:
+                pygame.draw.circle(screen, (255, 0, 0), (x_pos * 200 + 100, y_pos * 200 + 100), 80, line_width)
+            y_pos += 1
+        x_pos += 1
+
+
 run = True
 while run:
 
     draw_grid()
+    draw_markers()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.MOUSEBUTTONDOWN and not clicked:
+            clicked = True
+        if event.type == pygame.MOUSEBUTTONUP and clicked:
+            clicked = False
+            pos = pygame.mouse.get_pos()
+            cell_x = pos[0]
+            cell_y = pos[1]
+            if markers[cell_x // 200][cell_y // 200] == 0:
+                markers[cell_x // 200][cell_y // 200] = player
+                player *= -1
 
     pygame.display.update()
 
